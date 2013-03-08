b1ea5e303cc377dfd762a37024c37f95d476c098
Hendry Badao <hendrybadao@gmail.com>
2013-03-08 20:40:36 +0300

    Added new lang: Brazilian Portuguese

---
+++ lang/ptb/lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Portuguese Brazilian language file (translated by Benedito Carneiro <benedito.carneiro@gmail.com>)
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Symon Bent <symonbent@gmail.com>
+ *
+ */
+
+// custom language strings for the pagequery plugin
+$lang['no_results']   = 'Sem resultados para a pesquisa: « %1s »';
+$lang['jump_section'] = 'Continue com a seção « %1$s » no topo...'; // '%1$s' will be replaced by the relevant heading
+$lang['jump_to_top']  = 'Retorna ao topo da lista desta página';
+$lang['link_to_top']  = 'Topo ↑';
+$lang['regex_error']  = 'Tem um erro na expressão regular.  Verifique e tente novamente.';
+$lang['empty_filter'] = 'A expressão do filtro não retornou resultados.';
