e6766aba491274ae58caf7369d11edbf1ea01147
Thomas Kurschel <t.kurschel@asphericon.com>
2015-03-19 15:06:16 +0100

    fix caching

--- action.php
+++ action.php
@@ -9,6 +9,7 @@ class action_plugin_pagequery extends DokuWiki_Action_Plugin {
      */
     function register(Doku_Event_Handler $controller) {
         $controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button', array ());
+        $controller->register_hook('PARSER_CACHE_USE', 'BEFORE', $this, '_purgecache');
     }
 
 
@@ -38,6 +39,50 @@ class action_plugin_pagequery extends DokuWiki_Action_Plugin {
         $text .= '</div>' . PHP_EOL;
 
         return $text;
+    }
+
+
+    /**
+     * Check for pages changes and eventually purge cache.
+     *
+     * @author Samuele Tognini <samuele@samuele.netsons.org>
+     *
+     * @param Doku_Event $event
+     * @param mixed      $param not defined
+     */
+    function _purgecache(&$event, $param) {
+        global $ID;
+        global $conf;
+        /** @var cache_parser $cache */
+        $cache = &$event->data;
+
+        if(!isset($cache->page)) return;
+        //purge only xhtml cache
+        if($cache->mode != "xhtml") return;
+        //Check if it is an pagequery page
+        if(!p_get_metadata($ID, 'pagequery')) return;
+        $aclcache = $this->getConf('aclcache');
+        if($conf['useacl']) {
+            $newkey = false;
+            if($aclcache == 'user') {
+                //Cache per user
+                if($_SERVER['REMOTE_USER']) $newkey = $_SERVER['REMOTE_USER'];
+            } else if($aclcache == 'groups') {
+                //Cache per groups
+                global $INFO;
+                if($INFO['userinfo']['grps']) $newkey = implode('#', $INFO['userinfo']['grps']);
+            }
+            if($newkey) {
+                $cache->key .= "#".$newkey;
+                $cache->cache = getCacheName($cache->key, $cache->ext);
+            }
+        }
+        //Check if a page is more recent than purgefile.
+        if(@filemtime($cache->cache) < @filemtime($conf['cachedir'].'/purgefile')) {
+            $event->preventDefault();
+            $event->stopPropagation();
+            $event->result = false;
+        }
     }
 }
 
